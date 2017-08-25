548401d76b86f3f2fdce9366f2815db45a718d3a
zhangzhiqi <zhangzhiqi@kobox.tv>
2017-08-25 17:52:02 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	logging "github.com/op/go-logging"
+)
+
+var logger *logging.Logger
+
+func init() {
+	logger = logging.MustGetLogger("main")
+}
+
+func main() {
+	return
+}
